250fcf1111ae55e65c6ee97d53070c43855603ea
Ahmed Yasser <ahmed17morra@gmail.com>
2025-05-16 20:26:36 +0300

    add some props

--- backend/authentication-service/src/test/java/com/pro/authenticationservice/AuthenticationServiceApplicationTests.java
+++ backend/authentication-service/src/test/java/com/pro/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -8,6 +8,13 @@ import org.springframework.test.context.TestPropertySource;
 @TestPropertySource(properties = {
         // This property should disable the Spring Cloud Config client for these tests
         "spring.cloud.config.enabled=false",
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=LEGACY",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.show-sql=false",
+        "jwt.secret=${JWT_SECRET}"
 })
 
 class AuthenticationServiceApplicationTests {
